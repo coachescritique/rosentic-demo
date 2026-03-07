3b994cad8b6ff74d42bab82a65a8ea9b99f0f8e3
didyoudangle <admin@coachescritique.com>
2026-03-07 15:19:57 -0600

    Diana: add currency parameter to ProcessPayment

--- payment/service.go
+++ payment/service.go
@@ -2,7 +2,7 @@ package payment
 
 import "fmt"
 
-func ProcessPayment(amount float64, accountID string) (string, error) {
-	txnID := fmt.Sprintf("txn_%s_%.0f", accountID, amount)
+func ProcessPayment(amount float64, accountID string, currency string) (string, error) {
+	txnID := fmt.Sprintf("txn_%s_%.0f_%s", accountID, amount, currency)
 	return txnID, nil
 }
